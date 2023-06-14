4c51b16c87037998793409503c5137d0f1be5467
Nikola <nikola9988markovic@gmail.com>
2023-06-14 20:08:45 +0200

    perses

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,73 +1,84 @@
 export default [
-    {
-        title: "Incognito Chat",
-        text: "Chat application where you can generate a chat/room. Every chat has its own URL to it and anyone that has the link to it can enter and send messages and pictures.",
-        technologies: ["HTML", "SCSS", "Angular", "Go"],
-        links: [
-            {
-                to: "https://github.com/nikolamarkovic98/incognito-chat-front",
-                text: "Frontend Code",
-            },
-            {
-                to: "https://github.com/nikolamarkovic98/incognito-chat-api",
-                text: "Backend Code",
-            },
-            {
-                to: "https://incognito-chat.onrender.com/",
-                text: "Live",
-            },
-        ],
-    },
-    {
-        title: "testMe",
-        text: "testMe is platform where you can create your own tests that other people could try to solve. It's purpose is to provide education by enabling users to create their own tests and by completing tests from other users.",
-        technologies: [
-            "HTML",
-            "CSS",
-            "React",
-            "Node/Express",
-            "GraphQL",
-            "MongoDB",
-        ],
-        links: [
-            {
-                to: "https://github.com/nikolamarkovic98/testme-front",
-                text: "Frontend Code",
-            },
-            {
-                to: "https://github.com/nikolamarkovic98/testme-api",
-                text: "Backend Code",
-            },
-        ],
-    },
-    {
-        title: "Hungry Herceg",
-        text: "Hungry Herceg is food ordering applicatin that I made with colleagues from IT Bootcamp. You create poll with recommended list of restaurants and everyone votes for a restaurant that they would like to order food from. After poll ends, you make order from restaurant that had the most votes. My part in development of this application was on the backend. I worked on the API, interacted with database and created logic for automatic end of polls and orders.",
-        technologies: ["Node", "Express", "RestAPI", "MongoDB"],
-        links: [
-            {
-                to: "https://github.com/nikolamarkovic98/hungry-herceg-api",
-                text: "Code",
-            },
-            {
-                to: "https://hungry-herceg.netlify.app",
-                text: "Live",
-            },
-        ],
-    },
-    {
-        title: "Code Counter",
-        text: "CodeCounter is project analyzer. You can upload files or just provide Github repository and it's going to calculate how many lines, words and characters are there in your project for each type of file.",
-        technologies: ["Node", "Express", "VanillaJS"],
-        links: [
-            {
-                to: "https://github.com/nikolamarkovic98/codecounter",
-                text: "Code",
-            },
-            {
-                to: "https://code-counter.onrender.com/",
-                text: "Live",
-            },
-        ],
-    },
+  {
+    title: "Perses",
+    text: "Perses enables users to assess the financial performance of companies and countries, offering portfolio creation and diverse analytics. Integrated with Power BI, our platform showcases insightful dashboards displaying key data like GDP. Experience an intuitive interface, empowering investors and researchers to make informed decisions based on comprehensive economic analysis.",
+    technologies: ["NextJS", "Express", "Typescript", "Postgre", "Tailwind"],
+    links: [
+      {
+        to: "https://perses.onrender.com/",
+        text: "Live",
+      },
+    ],
+  },
+  {
+    title: "Incognito Chat",
+    text: "Chat application where you can generate a chat/room. Every chat has its own URL to it and anyone that has the link to it can enter and send messages and pictures.",
+    technologies: ["HTML", "SCSS", "Angular", "Go"],
+    links: [
+      {
+        to: "https://github.com/nikolamarkovic98/incognito-chat-front",
+        text: "Frontend Code",
+      },
+      {
+        to: "https://github.com/nikolamarkovic98/incognito-chat-api",
+        text: "Backend Code",
+      },
+      {
+        to: "https://incognito-chat.onrender.com/",
+        text: "Live",
+      },
+    ],
+  },
+  {
+    title: "testMe",
+    text: "testMe is platform where you can create your own tests that other people could try to solve. It's purpose is to provide education by enabling users to create their own tests and by completing tests from other users.",
+    technologies: [
+      "HTML",
+      "CSS",
+      "React",
+      "Node/Express",
+      "GraphQL",
+      "MongoDB",
+    ],
+    links: [
+      {
+        to: "https://github.com/nikolamarkovic98/testme-front",
+        text: "Frontend Code",
+      },
+      {
+        to: "https://github.com/nikolamarkovic98/testme-api",
+        text: "Backend Code",
+      },
+    ],
+  },
+  {
+    title: "Hungry Herceg",
+    text: "Hungry Herceg is food ordering applicatin that I made with colleagues from IT Bootcamp. You create poll with recommended list of restaurants and everyone votes for a restaurant that they would like to order food from. After poll ends, you make order from restaurant that had the most votes. My part in development of this application was on the backend. I worked on the API, interacted with database and created logic for automatic end of polls and orders.",
+    technologies: ["Node", "Express", "RestAPI", "MongoDB"],
+    links: [
+      {
+        to: "https://github.com/nikolamarkovic98/hungry-herceg-api",
+        text: "Code",
+      },
+      {
+        to: "https://hungry-herceg.netlify.app",
+        text: "Live",
+      },
+    ],
+  },
+  {
+    title: "Code Counter",
+    text: "CodeCounter is project analyzer. You can upload files or just provide Github repository and it's going to calculate how many lines, words and characters are there in your project for each type of file.",
+    technologies: ["Node", "Express", "VanillaJS"],
+    links: [
+      {
+        to: "https://github.com/nikolamarkovic98/codecounter",
+        text: "Code",
+      },
+      {
+        to: "https://code-counter.onrender.com/",
+        text: "Live",
+      },
+    ],
+  },
 ];
